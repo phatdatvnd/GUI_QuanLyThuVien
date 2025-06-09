b52c13e53117839e3543900c5645a0803b4e251c
bla bla <anhlhtb01547@gmail.com>
2025-06-09 16:13:43 +0700

    them doi mk

---
+++ GUI_QuanLyThuVien/frmdoimk.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace GUI_QuanLyThuVien
+{
+    partial class frmdoimk
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmdoimk));
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges11 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges12 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            label1 = new Label();
+            btnthoat = new Guna.UI2.WinForms.Guna2GradientButton();
+            btndangnhap = new Guna.UI2.WinForms.Guna2GradientButton();
+            guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            bamhienmatkhau = new Guna.UI2.WinForms.Guna2ToggleSwitch();
+            txtmatkhau = new Guna.UI2.WinForms.Guna2TextBox();
+            pictureBox1 = new PictureBox();
+            guna2TextBox1 = new Guna.UI2.WinForms.Guna2TextBox();
+            guna2TextBox2 = new Guna.UI2.WinForms.Guna2TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Black", 20F);
+            label1.Location = new Point(827, 132);
+            label1.Name = "label1";
+            label1.Size = new Size(285, 46);
+            label1.TabIndex = 15;
+            label1.Text = "ĐỔI MẬT KHẨU";
+            // 
+            // btnthoat
+            // 
+            btnthoat.BorderRadius = 20;
+            btnthoat.CustomizableEdges = customizableEdges1;
+            btnthoat.DisabledState.BorderColor = Color.DarkGray;
+            btnthoat.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnthoat.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnthoat.DisabledState.FillColor2 = Color.FromArgb(169, 169, 169);
+            btnthoat.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnthoat.Font = new Font("Times New Roman", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnthoat.ForeColor = Color.White;
+            btnthoat.Location = new Point(762, 546);
+            btnthoat.Margin = new Padding(3, 4, 3, 4);
+            btnthoat.Name = "btnthoat";
+            btnthoat.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            btnthoat.Size = new Size(385, 60);
+            btnthoat.TabIndex = 14;
+            btnthoat.Text = "Thoát";
+            // 
+            // btndangnhap
+            // 
+            btndangnhap.BorderRadius = 20;
+            btndangnhap.CustomizableEdges = customizableEdges3;
+            btndangnhap.DisabledState.BorderColor = Color.DarkGray;
+            btndangnhap.DisabledState.CustomBorderColor = Color.DarkGray;
+            btndangnhap.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btndangnhap.DisabledState.FillColor2 = Color.FromArgb(169, 169, 169);
+            btndangnhap.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btndangnhap.Font = new Font("Times New Roman", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btndangnhap.ForeColor = Color.White;
+            btndangnhap.Location = new Point(762, 466);
+            btndangnhap.Margin = new Padding(3, 4, 3, 4);
+            btndangnhap.Name = "btndangnhap";
+            btndangnhap.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            btndangnhap.Size = new Size(385, 60);
+            btndangnhap.TabIndex = 13;
+            btndangnhap.Text = "Đổi Mật Khẩu";
+            btndangnhap.Click += btndangnhap_Click;
+            // 
+            // guna2HtmlLabel1
+            // 
+            guna2HtmlLabel1.BackColor = Color.Transparent;
+            guna2HtmlLabel1.Location = new Point(827, 424);
+            guna2HtmlLabel1.Margin = new Padding(3, 4, 3, 4);
+            guna2HtmlLabel1.Name = "guna2HtmlLabel1";
+            guna2HtmlLabel1.Size = new Size(99, 22);
+            guna2HtmlLabel1.TabIndex = 12;
+            guna2HtmlLabel1.Text = "Hiện mật khẩu";
+            // 
+            // bamhienmatkhau
+            // 
+            bamhienmatkhau.CheckedState.BorderColor = Color.FromArgb(94, 148, 255);
+            bamhienmatkhau.CheckedState.FillColor = Color.FromArgb(94, 148, 255);
+            bamhienmatkhau.CheckedState.InnerBorderColor = Color.White;
+            bamhienmatkhau.CheckedState.InnerColor = Color.White;
+            bamhienmatkhau.CustomizableEdges = customizableEdges5;
+            bamhienmatkhau.Location = new Point(762, 424);
+            bamhienmatkhau.Margin = new Padding(3, 4, 3, 4);
+            bamhienmatkhau.Name = "bamhienmatkhau";
+            bamhienmatkhau.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            bamhienmatkhau.Size = new Size(58, 23);
+            bamhienmatkhau.TabIndex = 11;
+            bamhienmatkhau.UncheckedState.BorderColor = Color.FromArgb(125, 137, 149);
+            bamhienmatkhau.UncheckedState.FillColor = Color.FromArgb(125, 137, 149);
+            bamhienmatkhau.UncheckedState.InnerBorderColor = Color.White;
+            bamhienmatkhau.UncheckedState.InnerColor = Color.White;
+            bamhienmatkhau.CheckedChanged += bamhienmatkhau_CheckedChanged;
+            // 
+            // txtmatkhau
+            // 
+            txtmatkhau.BackColor = SystemColors.ButtonHighlight;
+            txtmatkhau.CustomizableEdges = customizableEdges7;
+            txtmatkhau.DefaultText = "";
+            txtmatkhau.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txtmatkhau.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txtmatkhau.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txtmatkhau.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txtmatkhau.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtmatkhau.Font = new Font("Segoe UI", 9F);
+            txtmatkhau.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtmatkhau.IconLeft = (Image)resources.GetObject("txtmatkhau.IconLeft");
+            txtmatkhau.Location = new Point(762, 352);
+            txtmatkhau.Margin = new Padding(3, 5, 3, 5);
+            txtmatkhau.Name = "txtmatkhau";
+            txtmatkhau.PlaceholderText = "Password";
+            txtmatkhau.SelectedText = "";
+            txtmatkhau.ShadowDecoration.CustomizableEdges = customizableEdges8;
+            txtmatkhau.Size = new Size(385, 63);
+            txtmatkhau.TabIndex = 10;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.Dock = DockStyle.Left;
+            pictureBox1.Image = Properties.Resources.Screenshot_2025_05_23_162244;
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Margin = new Padding(3, 4, 3, 4);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(517, 652);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 8;
+            pictureBox1.TabStop = false;
+            // 
+            // guna2TextBox1
+            // 
+            guna2TextBox1.BackColor = SystemColors.ButtonHighlight;
+            guna2TextBox1.CustomizableEdges = customizableEdges9;
+            guna2TextBox1.DefaultText = "";
+            guna2TextBox1.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            guna2TextBox1.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            guna2TextBox1.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            guna2TextBox1.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            guna2TextBox1.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            guna2TextBox1.Font = new Font("Segoe UI", 9F);
+            guna2TextBox1.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            guna2TextBox1.IconLeft = (Image)resources.GetObject("guna2TextBox1.IconLeft");
+            guna2TextBox1.Location = new Point(762, 183);
+            guna2TextBox1.Margin = new Padding(3, 5, 3, 5);
+            guna2TextBox1.Name = "guna2TextBox1";
+            guna2TextBox1.PlaceholderText = "Password";
+            guna2TextBox1.SelectedText = "";
+            guna2TextBox1.ShadowDecoration.CustomizableEdges = customizableEdges10;
+            guna2TextBox1.Size = new Size(385, 63);
+            guna2TextBox1.TabIndex = 16;
+            // 
+            // guna2TextBox2
+            // 
+            guna2TextBox2.BackColor = SystemColors.ButtonHighlight;
+            guna2TextBox2.CustomizableEdges = customizableEdges11;
+            guna2TextBox2.DefaultText = "";
+            guna2TextBox2.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            guna2TextBox2.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            guna2TextBox2.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            guna2TextBox2.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            guna2TextBox2.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            guna2TextBox2.Font = new Font("Segoe UI", 9F);
+            guna2TextBox2.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            guna2TextBox2.IconLeft = (Image)resources.GetObject("guna2TextBox2.IconLeft");
+            guna2TextBox2.Location = new Point(762, 268);
+            guna2TextBox2.Margin = new Padding(3, 5, 3, 5);
+            guna2TextBox2.Name = "guna2TextBox2";
+            guna2TextBox2.PlaceholderText = "Password";
+            guna2TextBox2.SelectedText = "";
+            guna2TextBox2.ShadowDecoration.CustomizableEdges = customizableEdges12;
+            guna2TextBox2.Size = new Size(385, 63);
+            guna2TextBox2.TabIndex = 17;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(647, 204);
+            label2.Name = "label2";
+            label2.Size = new Size(109, 23);
+            label2.TabIndex = 18;
+            label2.Text = "Mạt khẩu cũ";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(633, 292);
+            label3.Name = "label3";
+            label3.Size = new Size(123, 23);
+            label3.TabIndex = 19;
+            label3.Text = "Mật khẩu mới";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(580, 370);
+            label4.Name = "label4";
+            label4.Size = new Size(176, 25);
+            label4.TabIndex = 20;
+            label4.Text = "Xác nhận mật khẩu";
+            // 
+            // frmdoimk
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1200, 652);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(guna2TextBox2);
+            Controls.Add(guna2TextBox1);
+            Controls.Add(label1);
+            Controls.Add(btnthoat);
+            Controls.Add(btndangnhap);
+            Controls.Add(guna2HtmlLabel1);
+            Controls.Add(bamhienmatkhau);
+            Controls.Add(txtmatkhau);
+            Controls.Add(pictureBox1);
+            Name = "frmdoimk";
+            Text = "frmdoimk";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Guna.UI2.WinForms.Guna2GradientButton btnthoat;
+        private Guna.UI2.WinForms.Guna2GradientButton btndangnhap;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
+        private Guna.UI2.WinForms.Guna2ToggleSwitch bamhienmatkhau;
+        private Guna.UI2.WinForms.Guna2TextBox txtmatkhau;
+        private PictureBox pictureBox1;
+        private Guna.UI2.WinForms.Guna2TextBox guna2TextBox1;
+        private Guna.UI2.WinForms.Guna2TextBox guna2TextBox2;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+    }
+}
---
+++ GUI_QuanLyThuVien/frmdoimk.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BLL_QuanLyThuVien;
+using DAL_QuanLyThuVien;
+using Util_QuanLyThuVien;
+
+namespace GUI_QuanLyThuVien
+{
+    public partial class frmdoimk : Form
+    {
+        DALNhanVien DALNhanVien = new DALNhanVien();
+        public frmdoimk()
+        {
+            InitializeComponent();
+        }
+
+        private void btndangnhap_Click(object sender, EventArgs e)
+        {
+            if (!AuthUtil.user.MatKhau.Equals(guna2TextBox1.Text))
+            {
+                MessageBox.Show("Mật khẩu hiện tại không đúng");
+            }
+            // Kiểm tra mật khẩu mới và xác nhận có trùng nhau không
+            else if (!guna2TextBox2.Text.Equals(txtmatkhau.Text))
+            {
+                MessageBox.Show("Mật khẩu mới và xác nhận mật khẩu không trùng khớp");
+            }
+            else
+            {
+                // Thực hiện đổi mật khẩu
+                if (DALNhanVien.resetmatkhau(AuthUtil.user.Email, guna2TextBox2.Text))
+                {
+                    MessageBox.Show("Đổi mật khẩu thành công");
+                    AuthUtil.user.MatKhau = guna2TextBox2.Text; // cập nhật lại mật khẩu trong bộ nhớ
+                }
+                else
+                {
+                    MessageBox.Show("Đổi mật khẩu không thành công");
+                }
+            }
+        }
+
+        private void bamhienmatkhau_CheckedChanged(object sender, EventArgs e)
+        {
+            guna2TextBox1.UseSystemPasswordChar = !bamhienmatkhau.Checked;
+        }
+    }
+}
